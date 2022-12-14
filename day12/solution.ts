31a70caf63ff499706d85ffc38c60100adb37406
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2022-12-14 16:51:55 +0100

    add AOC2022 Day12 P1

--- day12/solution.ts
+++ day12/solution.ts
@@ -12,79 +12,39 @@ class Solution {
   private grid: string[][];
   private startPosition: number[];
   private endPosition: number[];
-  private visited: string[][];
-
-  static Visited = 'X';
-  static Open = '.';
+  private distances: number[][];
 
   constructor (grid: string[][]) {
     this.grid = grid;
     this.startPosition = this.getPositionOf('S')!;
     this.endPosition = this.getPositionOf('E')!;
-    this.visited = Array.from({ length: grid.length }, (_, i) => Array.from({ length: grid[i].length }, () => Solution.Open));
+    this.distances = Array.from({ length: grid.length }, (_, i) => Array.from({ length: grid[i].length }, () => Number.MAX_VALUE));
+    this.distances[this.startPosition[0]][this.startPosition[1]] = 0;
   }
 
   public solvePartOne() {
-    this.travel(this.startPosition);
-
-    this.drawVisited();
-
-    return this.calculatePath()
+    const toCheck = [[...this.startPosition, 0]];
+    const result = this.climb(toCheck);
+    
+    return result;
   }
 
-  public drawVisited() {  
-    return this.visited.forEach((row) => {
-      console.log(row.join(' '));
-    });
-  }
+  private climb = (toCheck: number[][]) => {
+    while (toCheck.length > 0) {
+      const [row, col, distance] = toCheck.shift()!;
 
-  private calculatePath () {
-    let length = 0;
-    for (const row of this.visited) {
-      length += row.filter((c) => ['^', 'v', '<', '>'].includes(c)).length;
-    }
-
-    return length;
-  }
-
-  private travel (position: number[]) {
-    if (!this.isValidMove(position)) {
-      return false;
-    }
-
-    if (this.isEnd(position)) {
-      this.visited[position[0]][position[1]] = this.getDirection(position);
-      return true;
-    } else {
-      this.visited[position[0]][position[1]] = Solution.Visited;
-    }
-
-    const moves = (this.getAvailableMoves(position) as number[][])
-      .sort(this.distanceComparator);
-
-    for (const move of moves) {
-      const [row, col] = move;
-      if (this.travel(move)) {
-        this.visited[row][col] = this.getDirection(move);
-        return true;
+      if (this.isEnd([row, col])) {
+        this.distances[row][col] = distance;
+        return this.distances[row][col];
       }
+
+      (<number[][]>this.getAvailableMoves([row, col])).forEach(([r, c]) => {
+        if (this.distances[r][c] === Number.MAX_VALUE) {
+          this.distances[r][c] = distance + 1;
+          toCheck.push([r, c, distance + 1]);
+        }
+      });
     }
-
-    return false;
-  }
-
-  private distanceComparator = (a: number[], b: number[]) => {
-    const aDistance = this.getDistance(a, this.endPosition) - this.getDistance(a, this.startPosition);
-    const bDistance = this.getDistance(b, this.endPosition) - this.getDistance(b, this.startPosition);
-
-    return aDistance - bDistance;
-  }
-
-  private getDistance (pos1: number[], pos2: number[]) {
-    const dx = pos1[0] - pos2[0];
-    const dy = pos1[1] - pos2[1];
-  
-    return Math.sqrt(dx * dx + dy * dy);
   };
 
   private getElevation (sign: string) {
@@ -116,29 +76,13 @@ class Solution {
     return atPosition[0] === this.endPosition[0] && atPosition[1] === this.endPosition[1];
   }
 
-  private isValidMove(atPosition: number[]) {
-    return this.visited[atPosition[0]][atPosition[1]] === Solution.Open;
-  }
-
-  private getDirection (move: number[]) {
-    const [_r, _c, dirCode] = move;
-    switch (dirCode) {
-      case 101: return '^';
-      case 102: return 'v';
-      case 103: return '<';
-      case 104: return '>';
-    }
-
-    return '?';
-  }
-
   private getAvailableMoves = (currentPosition: number[]) => {
     const [row, col] = currentPosition;
   
-    const up = row > 0 ? [row - 1, col, 101] : null;
-    const down = row < this.grid.length - 1 ? [row + 1, col, 102] : null;
-    const left = col > 0 ? [row, col - 1, 103] : null;
-    const right = col < this.grid[row].length - 1 ? [row, col + 1, 104] : null;
+    const up = row > 0 ? [row - 1, col] : null;
+    const down = row < this.grid.length - 1 ? [row + 1, col] : null;
+    const left = col > 0 ? [row, col - 1] : null;
+    const right = col < this.grid[row].length - 1 ? [row, col + 1] : null;
   
     const signAtCurrentPosition = this.grid[row][col];
     const currentElevation = this.getElevation(signAtCurrentPosition);
@@ -147,9 +91,6 @@ class Solution {
       if (move === null) return false;
   
       const [r, c] = move;
-      if (this.visited[r][c] !== Solution.Open) return false;
-      if (r === this.startPosition[0] && c === this.startPosition[1]) return false;
-
       const sign = this.grid[r][c];
       const elevationAtMove = this.getElevation(sign);
   
@@ -161,14 +102,13 @@ class Solution {
   };
 }
 
-const solvePartOne = async () => {
-  const input = await getInput();
-
-  const solution = new Solution(input);
-
-  return solution.solvePartOne();
-};
 
 (async () => {
-  console.log(`1. ${await solvePartOne()}`);
+  const input = await getInput();
+
+  const s1 = new Solution([...input]);
+  console.log(`1. ${s1.solvePartOne()}`);
+
+  const s2 = new Solution([...input]);
+  console.log(`1. ${s2.solvePartOne()}`);
 })();
